6a94d9f0e496a9e0932cb6f0cae214f085d4296d
Jesse Moore <moore3tj@yahoo.com>
2022-09-07 14:13:38 -0500

    commit and push

---
+++ Python/Intoduction/Python If-Else.py
@@ -0,0 +1,43 @@
+"""
+Task
+Given an integer, , perform the following conditional actions:
+
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than , print Not Weird
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+1 <= n <= 100
+Output Format
+
+Print Weird if the number is weird. Otherwise, print Not Weird.
+
+Sample Input 0:
+3
+Sample Output 0:
+Weird
+Explanation 0:
+n is odd and odd numbers are weird, so print Weird.
+
+Sample Input 1:
+24
+Sample Output 1:
+Not Weird
+Explanation 1:
+n > 20 and n is even, so it is not weird."""
+
+if __name__ == '__main__':
+    n = int(input().strip())
+if n % 2 != 0:
+    print("Weird")
+elif n in range(2,5) and n % 2 ==0:
+    print("Not Weird")
+elif n in range(6,21) and n % 2 ==0:
+    print("Weird")
+elif n > 20 and n % 2 ==0:
+    print("Not Weird")
+
